23dce788c6d8ac40067ac646646f99dcfa88b163
elenakomaritska <komaritskaelena@gmail.com>
2020-08-28 21:51:03 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,6 +64,7 @@ $(document).ready(function () {
         $('.close').on('click', function () {
             $(".modall").removeClass('active-modall');
             $('.body').removeClass('showing-modal');
+            $(".modall-video iframe").attr('src', '')
         });
 
     $('.link-video').on('click', function () {
